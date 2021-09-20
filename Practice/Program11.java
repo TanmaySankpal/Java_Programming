ea5e4ded1cccd20b5541697f4ecd67bc1950ec96
Tanmay Sankpal <90703196+TanmaySankpal@users.noreply.github.com>
2021-09-20 23:11:22 +0530

    Add files via upload

---
+++ Practice/Program11.java
@@ -0,0 +1,13 @@
+//Print 5 times "Marvellous"
+import java.lang.*;
+class Program11
+{
+	public static void main(String arg[])
+	{
+		int iCnt=0;
+		for(iCnt=1;iCnt<=5;iCnt++)
+		{
+			System.out.println("Marvellous");
+		}
+	}
+}
