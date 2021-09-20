e93402b0155b414612079061b9f1458853905273
Tanmay Sankpal <90703196+TanmaySankpal@users.noreply.github.com>
2021-09-20 23:05:37 +0530

    Add files via upload

---
+++ Practice/Program10.java
@@ -0,0 +1,17 @@
+//print 8 times "#"
+import java.lang.*;
+class Program10
+{
+	public static void main(String arg[])
+	{
+		Disply();
+	}
+	public static void Disply()
+	{
+		int iCnt=0;
+		for(iCnt=1;iCnt<=8;iCnt++)
+		{
+			System.out.println("#");
+		}
+	}
+}
