85052152891152ed8fcfd7f9189a461dfb6269c3
Tanmay Sankpal <90703196+TanmaySankpal@users.noreply.github.com>
2021-09-20 23:04:15 +0530

    Add files via upload

---
+++ Practice/Program7.java
@@ -0,0 +1,17 @@
+// Display 1 to 5 on screen.
+class Program7
+{
+	public static void main(String arg[])
+	{
+		Display();
+
+	}
+	public static void Display()
+	{
+		int iCnt=0;
+		for(iCnt=1;iCnt<=5;iCnt++)
+		{
+			System.out.println(""+iCnt);
+		}
+	}
+}
